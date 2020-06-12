cb7ae8c4d271d509ab82a7cca4e8b85ff12f4491
datwaft <datwaftx@gmail.com>
2020-06-11 22:21:04 -0600

    login v1

---
+++ src/main/webapp/resources/js/login.js
@@ -0,0 +1,71 @@
+/*  NOTA IMPORTANTE 
+ *  
+ *  En la variable global llamada 'data' es donde se guardan los datos de esta página web,
+ *  con solo modificarla ya se modifican sus datos.
+ *
+ *  Ejemplo: 
+ *    Para actualizar las categorias basta con hacer `data.categories = [...]` y con eso ya
+ *    se renderizan las nuevas categorías.
+ *
+ *  Adicionalmente, voy a poner una descripción a cada atributo del data para saber qué
+ *  hace cada uno y qué utilidad posee.
+ *
+ *  Pd: Abajo del todo está la función donde se hacen las llamadas a la BD al inicio del
+ *  programa.
+ */ 
+
+/*  ¡¡NOTA IMPORTANTE!!
+ *  
+ *  A la hora de hacer login se puede hacer usando data.email y data.password, esos siempre
+ *  poseen los datos del usuario.
+ */
+
+var data = {
+  /*  Esta variable guarda la dirección de email del usuario.
+   *  Se actualiza de manera automática.
+   */
+  email: '',
+  /*  Esta variable guarda la contraseña del usuario.
+   *  Se actualiza de manera automática.
+   */
+  password: ''
+};
+
+/* ============================================================================================ 
+ * INICIO DE LA PROGRAMACIÓN RELACIONADA CON VUEJS
+ * ============================================================================================
+ */
+
+var vmLogIn = new Vue({
+  el: '#login',
+  data: data,
+  computed: {
+    isEmailValid: function () {
+      var conditions = [
+        this.email.length > 0,
+        this.email.length <= 35
+      ];
+      return conditions.every((e) => e);
+    },
+    isPasswordValid: function () {
+      var conditions = [
+        this.password.length > 0,
+        this.password.length <= 64
+      ];
+      return conditions.every((e) => e);
+    }
+  }
+});
+
+/* ============================================================================================ 
+ * FIN DE LA PROGRAMACIÓN RELACIONADA CON VUEJS
+ * ============================================================================================
+ */
+
+/*  NOTA IMPORTANTE
+ *
+ *  Aquí se hacen las llamadas a la BD al inicio del programa.
+ */
+
+$(window).on('load', async () => {
+});
---
+++ src/main/webapp/user/login/index.jsp
@@ -0,0 +1,33 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+  <%@ include file="/head.jsp" %> 
+  <link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/login.css">
+  <title>Login - TastyTasty</title>
+</head>
+<body>
+  <%@ include file="/header.jsp" %> 
+  <main class="body">
+      <div class="box">
+        <!-- START Vue: vmLogIn -->
+        <div id="login">
+          <h3 class="title">Log In</h3>
+          <div class="input-group">
+            <input type="text" v-model="email" placeholder="Email Address" :class="{invalid: !isEmailValid}">
+            <div><i class="fas fa-at"></i></div>
+          </div>
+          <div class="input-group">
+            <input type="password" v-model="password" placeholder="Password" :class="{invalid: !isPasswordValid}">
+            <div><i class="fas fa-asterisk"></i></div>
+          </div>
+          <button class="button">Login</button>
+          <button class="alt-button" onclick="location.href = '${pageContext.request.contextPath}/user/register';">Register</button>
+        </div>
+        <!-- END Vue: vmLogIn -->
+      </div>
+  </main>
+  <%@ include file="/footer.jsp" %> 
+</body>
+<script src="${pageContext.request.contextPath}/resources/js/login.js"></script>
+</html>
