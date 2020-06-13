64341b0cdd708827b21e955ded9dc23d045ec5c7
datwaft <datwaftx@gmail.com>
2020-06-12 23:38:47 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,6 +1,10 @@
 insert into `admin`(`username`, `password`) values
 ('admin', 'password');
 
+insert into `user`(`email`, `password`, `firstName`, `lastName`, `telephone`) values
+('user@mail.com', 'password', 'Default', 'User', '12345678'),
+('david@mail.com', 'password', 'David', 'Guevara', '86081212'); 
+
 insert into `category`(`description`) values
 ('Appetizer'),
 ('Main Course'),
