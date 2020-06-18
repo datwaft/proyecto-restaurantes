7c3e36a283531fa931f60b3e8687e530cfb25ed3
datwaft <datwaftx@gmail.com>
2020-06-17 23:12:50 -0600

    Fix with AutoIncrement

--- src/main/java/app/logic/Bill.java
+++ src/main/java/app/logic/Bill.java
@@ -8,6 +8,8 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -36,8 +38,8 @@ public class Bill implements Serializable {
 
   private static final long serialVersionUID = 1L;
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Basic(optional = false)
-  @NotNull
   @Column(name = "id")
   private Integer id;
   @Size(max = 45)
